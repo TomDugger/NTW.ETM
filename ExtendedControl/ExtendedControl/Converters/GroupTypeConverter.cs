a2a0bfa4032a677672d0024c8f90432009ecc383
TomDugger <dugger13@mail.ru>
2018-10-19 17:27:24 +0300

    non reliase

---
+++ ExtendedControl/ExtendedControl/Converters/GroupTypeConverter.cs
@@ -0,0 +1,28 @@
+﻿using NTW;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ExtendedControl.Converters
+{
+    public class GroupTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var parent = Common.FindAncestor<GroupItem>(value as DependencyObject);
+            if (targetType == typeof(string))
+                return false;
+            else return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
