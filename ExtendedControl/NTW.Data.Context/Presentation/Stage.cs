343032446649376e6bbe19455e63733f9df650ec
TomDugger <dugger13@mail.ru>
2018-11-04 16:23:20 +0300

    presentation mode for stage

---
+++ ExtendedControl/NTW.Data.Context/Presentation/Stage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTW.Data.Context//.Presentation
+{
+    public partial class Stage
+    {
+        public TimeSpan EndTime {
+            get { return this.DeadLine.TimeOfDay; }
+            set {
+                this.DeadLine = new DateTime(this.DeadLine.Year, this.DeadLine.Month, this.DeadLine.Day, value.Hours, value.Minutes, value.Seconds);
+                this.OnPropertyChanged(nameof(EndTime));
+                this.OnPropertyChanged(nameof(DeadLine));
+            }
+        }
+    }
+}
