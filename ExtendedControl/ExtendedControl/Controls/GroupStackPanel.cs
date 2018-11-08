f86c157907b0718d90c5fdf964e5eac315d653a2
TomDugger <dugger13@mail.ru>
2018-11-08 18:50:14 +0300

    group stack panel

---
+++ ExtendedControl/ExtendedControl/Controls/GroupStackPanel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ExtendedControl.Controls
+{
+    public class GroupStackPanel: StackPanel
+    {
+        public GroupStackPanel():base() {
+            this.Loaded += (s, e) => {
+                if (s is StackPanel && (s as StackPanel).Children.Count > 0
+            && (((s as StackPanel).Children[0] as GroupItem).DataContext as CollectionViewGroup).Name is DateTime)
+                {
+                    StackPanel panel = (StackPanel)s;
+                    panel.Orientation = Orientation.Vertical;
+                }
+            }; 
+        }
+    }
+}
